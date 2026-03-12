1972b378da3ce0874f8e12d08146f5c81f66e14e
RuchaDilipYerunkar <24yerunkarr@rbunagpur.in>
2026-03-12 14:22:40 +0530

    Add files via upload

---
+++ day 1/p6.py
@@ -0,0 +1,7 @@
+num = 123
+a = num % 10
+num = num // 10 #int
+b = num % 10
+c = num // 2
+rev = a*100 + b*10 + c*1
+print(rev)
---
+++ day 1/p9.py
@@ -0,0 +1,9 @@
+math = 50
+chem= 50
+print(id(math))
+print(id(chem))
+
+
+
+
+
